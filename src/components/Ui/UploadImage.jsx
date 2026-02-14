d7caaf3fde32bc19344e519f1f49255b95dd8a71
AZEEM KHAN <azeemkh778@gmail.com>
2026-02-14 18:17:31 +0500

    fixed

--- src/components/Ui/UploadImage.jsx
+++ src/components/Ui/UploadImage.jsx
@@ -1,61 +1,97 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react';
 import { EditFilled } from '@ant-design/icons';
-import { Button, Flex } from 'antd';
+import { Button, Flex, Spin } from 'antd';
 import { SingleFileUpload } from '../Forms';
 import { uploadFileToServer } from '../../services';
+import { TableLoader } from '../../shared';
 
-const UploadImage = ({setPreviewImage,src,t,form,disabled=false,api=null}) => {
+const UploadImage = ({
+  setPreviewImage,
+  src,
+  t,
+  form,
+  disabled = false,
+  api = null
+}) => {
 
-    const fileInputRef = useRef(null);
-    const handleFileChange = (e) => {
-        const file = e.target.files?.[0];
-        if (file) {
-            uploadFileToServer({ file, setPreviewImage });
-        }
-    };
-    const handleEditClick = () => {
-        fileInputRef.current?.click();
-    };
-    return (
-        <>
-            <input
-                ref={fileInputRef}
-                type="file"
-                accept="image/*"
-                onChange={handleFileChange}
-                className='hidden'
-            />
-            {
-                !src ?
-                    <SingleFileUpload
-                        name="document"
-                        title={t('Upload Image')}
-                        form={form}
-                        onUpload={(file)=>uploadFileToServer({file,setPreviewImage})}
-                        align="center"
-                        width={100}
-                        height={100}
-                        acceptFileType='image'
-                        api={api}
-                    />
-                    :
-                    <Flex vertical gap={5} justify='center' align='center'>
-                        <img
-                            src={src}
-                            alt="Category"
-                            className='radius-12 mxw-mxh'
-                            fetchPriority="high"
-                        />
-                        <div>
-                            <Button disabled={disabled} type="link" className='fs-13 text-brand' onClick={handleEditClick}>
-                                <EditFilled /> {t('Edit')}
-                            </Button>
-                        </div>
-                    </Flex>
+  const fileInputRef = useRef(null);
+  const [loading, setLoading] = useState(false);
 
-            }
-        </>
-    )
-}
+  const handleFileChange = async (e) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      try {
+        setLoading(true);
+        await uploadFileToServer({ file, setPreviewImage });
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
 
-export {UploadImage}
+  const handleEditClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleUpload = async (file) => {
+    try {
+      setLoading(true);
+      await uploadFileToServer({ file, setPreviewImage });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const hasValidImage = !!src && src !== 'null';
+
+  return (
+    <>
+      <input
+        ref={fileInputRef}
+        type="file"
+        accept="image/*"
+        onChange={handleFileChange}
+        hidden
+      />
+
+      {loading ? (
+        <Flex justify="center" align="center" style={{ height: '100px' }}>
+          <Spin {...TableLoader} size='small' />
+        </Flex>
+      ) : !hasValidImage ? (
+        <SingleFileUpload
+          name="document"
+          title={t('Upload Image')}
+          form={form}
+          onUpload={handleUpload}
+          align="center"
+          width={100}
+          height={100}
+          acceptFileType="image"
+          api={api}
+        />
+      ) : (
+        <Flex vertical gap={5} justify="center" align="center">
+          <img
+            src={src}
+            alt="Category"
+            className="radius-12 mxw-mxh"
+            fetchPriority="high"
+          />
+          <div>
+            <Button
+              disabled={disabled}
+              type="link"
+              className="fs-13 text-brand"
+              onClick={handleEditClick}
+            >
+              <EditFilled /> {t('Edit')}
+            </Button>
+          </div>
+        </Flex>
+      )}
+    </>
+  );
+};
+
+export { UploadImage };
